59769771da6754209b7aa150043260c84e638ece
sergey <fedulov_sy@mail.ru>
2022-12-17 01:14:16 +0300

    fist programm

---
+++ max_int/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число а ");
+int a = int.Parse(Console.ReadLine ());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine ());
+
+if (b>a) 
+{
+    Console.Write ("Максимальное число =");
+    Console.WriteLine (b);
+} 
+else
+{
+    Console.Write ("Максимальное число =");
+    Console.WriteLine (a);
+}
