1a8ff52f07a26b2d61ae3258d42b37675ff4490f
sergey <fedulov_sy@mail.ru>
2022-12-18 14:32:11 +0300

    create new programm3

--- max_3/Program.cs
+++ max_3/Program.cs
@@ -2,8 +2,8 @@
 Console.Write("Введите число и текст через запятую: ");
 string[] tokens = Console.ReadLine().Split(',');
 int number = int.Parse(tokens[0]);
-string text = tokens[1].Trim(); // тримминг пробела, если введено через запятую и пробел
-
+string text = tokens[1].Trim(); 
+if ()
 { 
 Console.Write ("число [{0}]", i);
 int [i] = int.Parse(Console.ReadLine ());
