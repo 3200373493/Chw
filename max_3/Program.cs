16686a5dbe0a0e68070cb2fea9dff24cfe3f0c6c
sergey <fedulov_sy@mail.ru>
2022-12-19 22:42:09 +0300

    max3

--- max_3/Program.cs
+++ max_3/Program.cs
@@ -1,10 +1,20 @@
 ﻿Console.Clear();
-Console.Write("Введите число и текст через запятую: ");
-string[] tokens = Console.ReadLine().Split(',');
-int number = int.Parse(tokens[0]);
-string text = tokens[1].Trim(); 
-if ()
-{ 
-Console.Write ("число [{0}]", i);
-int [i] = int.Parse(Console.ReadLine ());
+Console.Write("Введите три числа. Первое число= ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число= ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число= ");
+int c = int.Parse(Console.ReadLine());
+
+if (a>b && a>c) 
+{
+Console.WriteLine($"Максимальное число ={a}");
+} 
+else if(b>a && b>c)
+{
+Console.WriteLine($"Максимальное число ={b}");
+}
+else 
+{
+Console.WriteLine($"Максимальное число ={c}");
 }
